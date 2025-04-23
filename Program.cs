90016bbf7c5fa62d33ec3d3df5a6197abcc8580f
Baykes <williamdotbaker@gmail.com>
2025-04-23 10:16:15 -0700

    change api path

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var app = builder.Build();
 // Add services to the contain
 
 app.Urls.Add("http://0.0.0.0:8080");
-app.MapGet("/", () =>
+app.MapGet("/api/", () =>
 {
     return "This is my API";
 })
